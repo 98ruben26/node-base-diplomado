67f98f9ea0c4e276426467db91d8e421a593bf13
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-10 22:02:07 -0400

    Add authentication and tasks routes to app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import morgan from 'morgan';
-import usersRoutes from './routes/users.route.js'
+import usersRoutes from './routes/users.route.js';
+import authRoutes from './routes/auth.route.js';
+import tasksRoutes from './routes/tasks.route.js';
+
 const app = express();
 
-// Middlewares
+app.use(express.json());
 app.use(morgan('combined'));
 
-// Routes
-app.use('/', usersRoutes)
+app.use('/', usersRoutes);
+app.use('/', authRoutes);
+app.use('/', tasksRoutes);
 
 export default app;
+
