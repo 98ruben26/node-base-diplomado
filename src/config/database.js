d6052da4c13eaecc2eb1448a41ba1120ae371f76
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-10 21:45:40 -0400

    diplomados

---
+++ src/config/database.js
@@ -0,0 +1,25 @@
+import { Sequelize } from 'sequelize';
+//import dotenv from 'dotenv';
+import env from './env.js';
+//dotenv.config();
+
+
+
+const dialectOptions = env.db.useSSL
+  ? { ssl: { require: true, rejectUnauthorized: false } }
+  : {};
+
+const sequelize = new Sequelize(
+  env.db.database,
+  env.db.user,
+  env.db.password,
+  {
+    host: env.db.host,
+    port: env.db.port,
+    dialect: env.db.dialect,
+    dialectOptions,
+    logging: false,
+  }
+);
+
+export default sequelize;
