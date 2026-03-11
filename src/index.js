514ad38fa40df0a1e6a0b9852564392828df6323
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-10 22:03:20 -0400

    Refactor main function to handle async operations

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
+import 'dotenv/config'
+//import dotenv from 'dotenv';
 import app from './app.js';
 import env from './config/env.js';
 import logger from './logs/logger.js';
+import sequelize from './config/database.js'; // Sin llaves {} porque es export default
+//import { sequelize } from './models/index.js';
 
-function main() {
+async function main() {
+  await sequelize.authenticate();
+  logger.info('DB conectada correctamente');
+  await sequelize.sync({ alter: true });
+  logger.info('Tablas sincronizadas');
+
   const port = env.port;
-  app.listen(port)
+  app.listen(port);
   logger.info('Server on port ' + port);
 }
 
-main();
+main().catch((err) => {
+  logger.error('Error al iniciar: ' + err.message);
+  process.exit(1);
+});
