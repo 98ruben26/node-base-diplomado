17402d37e036c9086284eb62870745387d4cb7ac
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-12 00:39:15 -0400

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,6 @@
-import { Sequelize } from 'sequelize';
+//import { Sequelize } from 'sequelize';
 //import dotenv from 'dotenv';
+require('dotenv').config();
 import env from './env.js';
 //dotenv.config();
 
