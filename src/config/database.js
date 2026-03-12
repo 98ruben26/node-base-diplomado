be8f059b833163b7d06e63eed3af5d236867fa5d
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-12 00:39:56 -0400

    Refactor database.js to use Sequelize import

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 //import { Sequelize } from 'sequelize';
 //import dotenv from 'dotenv';
-require('dotenv').config();
+const { Sequelize } = require('sequelize');
 import env from './env.js';
 //dotenv.config();
 
