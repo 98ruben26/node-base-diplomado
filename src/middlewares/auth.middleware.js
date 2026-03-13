92ab7835b0ccd91d3f35b6be8ceb51f1bba88f70
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-12 20:41:39 -0400

    Refactor jsonwebtoken import syntax
    Changed import statement for jsonwebtoken to use require syntax.

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
-import jwt from 'jsonwebtoken';
+//import jwt from 'jsonwebtoken';
+const { jwt } = require('jsonwebtoken')
 import env from '../config/env.js';
 
 const verifyToken = (req, res, next) => {
