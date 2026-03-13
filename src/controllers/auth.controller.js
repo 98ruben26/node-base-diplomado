8c00f6d65afc9f3a3f1588919f06d186c0646d9c
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-12 20:36:10 -0400

    Refactor bcrypt import in auth.controller.js
    Changed bcrypt import method to use require.

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+//import bcrypt from 'bcrypt';
+const { bcrypt } = require('bcrypt');
 import { User } from '../models/index.js';
 import env from '../config/env.js';
 
