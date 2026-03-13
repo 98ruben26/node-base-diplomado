c2415c11daa07618a39d74c02b1ade7da7be8d93
98ruben26 <60021308+98ruben26@users.noreply.github.com>
2026-03-12 20:51:36 -0400

    Change import style for DataTypes in User model

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
-//import { DataTypes } from 'sequelize';
-const { DataTypes } = require('sequelize');
+import { DataTypes } from 'sequelize';
+//const { DataTypes } = require('sequelize');
 import sequelize from '../config/database.js';
 
 const User = sequelize.define('User', {
